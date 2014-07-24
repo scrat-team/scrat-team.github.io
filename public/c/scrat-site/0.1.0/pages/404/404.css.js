5cabf8af893ae74b5637ddba7d972120de310e66
travis-ci <travis@travis-ci.org>
2014-07-24 14:24:00 +0000

    [ci skip] publish website

--- public/c/scrat-site/0.1.0/pages/404/404.css.js
+++ public/c/scrat-site/0.1.0/pages/404/404.css.js
@@ -1,1 +1,1 @@
-require.defineCSS('scrat-site/0.1.0/pages/404/404.css', ".not-found{}.not-found{width:100%;height:100%;background:url(/public/c/scrat-site/0.1.0/pages/404/css-tricks-404_a35cabc.png) no-repeat center center}");
+require.defineCSS('scrat-site/0.1.0/pages/404/404.css', ".not-found{}.not-found{width:100%;height:100%;background:url(/public/c/scrat-site/0.1.0/pages/404/css-tricks-404_46d82f1.png) no-repeat center center}");
